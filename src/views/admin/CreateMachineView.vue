c2e43b8b400fe786b91045d3b43dea8689b05894
Andres Sebastian Sanchez Quintero <asanchezq@unal.edu.co>
2026-05-18 13:12:47 -0500

    Poner menu de pantalla de registro de maquina

--- src/views/admin/CreateMachineView.vue
+++ src/views/admin/CreateMachineView.vue
@@ -1,3 +1,17 @@
-<template>
-  <h1>Registrar máquina</h1>
+﻿<template>
+  <section class="create-machine-view">
+    <MachineForm />
+  </section>
 </template>
+
+<script setup>
+import MachineForm from '../../components/machines/MachineForm.vue'
+</script>
+
+<style scoped>
+.create-machine-view {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+}
+</style>
