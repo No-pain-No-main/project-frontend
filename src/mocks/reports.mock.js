e29fe3c65b5473a0180338adf2b894d5bae928f4
Andres Sebastian Sanchez Quintero <asanchezq@unal.edu.co>
2026-05-18 13:20:48 -0500

    para implementar pantalla de reportes

---
+++ src/mocks/reports.mock.js
@@ -0,0 +1,22 @@
+﻿export const usageReportsMock = [
+  {
+    machineId: 'ELP-001',
+    machineName: 'Elíptica Pro',
+    totalReservations: 142
+  },
+  {
+    machineId: 'CAM-04',
+    machineName: 'Caminadora',
+    totalReservations: 98
+  },
+  {
+    machineId: 'BIC-02',
+    machineName: 'Bicicleta estática',
+    totalReservations: 76
+  },
+  {
+    machineId: 'PRE-07',
+    machineName: 'Prensa de piernas',
+    totalReservations: 54
+  }
+]
