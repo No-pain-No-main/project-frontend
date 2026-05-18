52e391cc4d0819fba732ef586dcb1f92d4f055af
Andres Sebastian Sanchez Quintero <asanchezq@unal.edu.co>
2026-05-18 12:54:58 -0500

    para implementar pantalla de registro de usuario

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,3 +1,20 @@
-<template>
-  <h1>Registro de usuario</h1>
+﻿<template>
+  <section class="register-view">
+    <RegisterForm />
+  </section>
 </template>
+
+<script setup>
+import RegisterForm from '../../components/auth/RegisterForm.vue'
+</script>
+
+<style scoped>
+.register-view {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f4f6f8;
+  padding: 32px 24px;
+}
+</style>
